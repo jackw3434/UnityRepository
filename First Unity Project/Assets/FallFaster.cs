7fa1487a50c2aca0594e087788ae2da3ef3e316c
Jack <Jackw3434@gmail.com>
2018-10-28 00:09:13 +0100

    First Commit
    First Commit

---
+++ First Unity Project/Assets/FallFaster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallFaster : MonoBehaviour {
+
+    public Rigidbody rb;
+    public Transform Transform;
+    public int i;
+    public int f = 200;
+    // Use this for initialization
+    void Start() {
+        for (i = 1; i < f; i++ ) {
+            rb.AddForce(0, 1, 0);
+            Debug.Log("The number is " + i);
+            Debug.Log("The position is " + transform.position);
+        }
+        if (i == 200)
+        {
+            rb.AddForce(0, -200, 0);
+            transform.position = new Vector3(0, 0, 0);
+            transform.localScale = new Vector3(10, 10, 10);         
+        }
+       // rb.AddForce(0,-1000,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+       
+    }
+}
---
+++ First Unity Project/Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+       // Debug.Log("Hello World!");
+        // rb.useGravity = false;
+        //rb.AddForce(0,200,500);
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+       // rb.AddForce(0, 0, 2000 * Time.deltaTime);
+	}
+
+    void FixedUpdate()    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
