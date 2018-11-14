cac2ec7e78157e3863f0cbee5419b387efeab202
sonnytaylor <sonnytaylor5@gmail.com>
2018-11-14 20:58:58 +0000

    added touch joystick

--- 2D-Movement/2D Movement/Assets/LoadLevel.cs
+++ 2D-Movement/2D Movement/Assets/LoadLevel.cs
@@ -9,6 +9,7 @@ public class LoadLevel : MonoBehaviour {
 	public TileBase grassTile;
 	public TileBase dirtTile;
 	public TileBase underGroundTile;
+	public TileBase[] floatingTiles;
 	public TileBase[] rockTiles;
 	public Tilemap tilemap;
 	public Tilemap deathTileMap;
@@ -20,11 +21,9 @@ public class LoadLevel : MonoBehaviour {
 		for (int i = 0; i < 64; i++)
 		{
 			tilemap.SetTile(new Vector3Int(Convert.ToInt32(i * 1.0f), 0, 0), grassTile);
-			if (UnityEngine.Random.Range(0, 100) > 90)
-			{
-				Instantiate(cherry, new Vector3Int(Convert.ToInt32(i * 1.0f), 1, 0), this.transform.rotation, this.transform);
-			}
 
+			var firas = i;
+			Debug.Log(firas);
 			for (int j = 1; j < 4; j++)
 			{
 				if (UnityEngine.Random.Range(0, 100) > 90)
@@ -35,6 +34,17 @@ public class LoadLevel : MonoBehaviour {
 				{
 					tilemap.SetTile(new Vector3Int(i, Convert.ToInt32(j * -1.0f), 0), underGroundTile);
 				}
+			}
+		}
+
+		for (int i = 10; i <= 50; i += UnityEngine.Random.Range(5, 10))
+		{
+			tilemap.SetTile(new Vector3Int(Convert.ToInt32(i * 1.0f), 3, 0), floatingTiles[0]);
+			tilemap.SetTile(new Vector3Int(Convert.ToInt32(i+1 * 1.0f), 3, 0), floatingTiles[1]);
+			tilemap.SetTile(new Vector3Int(Convert.ToInt32(i+2 * 1.0f), 3, 0), floatingTiles[2]);
+			if (UnityEngine.Random.Range(0, 100) > 70)
+			{
+				Instantiate(cherry, new Vector3Int(Convert.ToInt32(i+1 * 1.0f), 4, 0), this.transform.rotation, this.transform);
 			}
 		}
 
