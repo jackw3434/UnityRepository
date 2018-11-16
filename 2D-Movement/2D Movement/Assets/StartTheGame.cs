7abf4def79e789a907838ba8e7854471aac32105
Jack <Jackw3434@gmail.com>
2018-11-16 19:42:01 +0000

    button onpress/menu scene

---
+++ 2D-Movement/2D Movement/Assets/StartTheGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartTheGame : MonoBehaviour {
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Main");
+        Debug.Log("Clicked");
+    }
+
+}
